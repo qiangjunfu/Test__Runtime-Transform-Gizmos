4a9c35e61a4671a70a525524106476ef0056e6d9
fuqiangjun <17611473176@163.com>
2025-01-20 16:07:40 +0800

    实现点击鼠标选中物体 , 控制物体移动旋转

---
+++ Assets/Scripts/Demo2.cs
@@ -0,0 +1,127 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using RTG;
+using UnityEngine;
+
+public class Demo2 : MonoBehaviour
+{
+    private GameObject selectedObject;  // 当前选中的物体
+
+
+    private void Start()
+    {
+        //// Create transform gizmos
+        //var moveTargetNames = new string[] { "Blue Cube", "Sphere" };
+        //foreach (var targetName in moveTargetNames)
+        //{
+        //    var transformGizmo = RTGizmosEngine.Get.CreateObjectMoveGizmo();
+
+        //    GameObject targetObject = GameObject.Find(targetName);
+        //    transformGizmo.SetTargetObject(targetObject);
+        //    transformGizmo.Gizmo.MoveGizmo.SetVertexSnapTargetObjects(new List<GameObject> { targetObject });
+        //    transformGizmo.SetTransformSpace(GizmoSpace.Local);
+        //}
+
+        //var rotationTargetNames = new string[] { "Cylinder", "Red Cube" };
+        //foreach (var targetName in rotationTargetNames)
+        //{
+        //    var transformGizmo = RTGizmosEngine.Get.CreateObjectRotationGizmo();
+
+        //    GameObject targetObject = GameObject.Find(targetName);
+        //    transformGizmo.SetTargetObject(targetObject);
+        //    transformGizmo.SetTransformSpace(GizmoSpace.Local);
+        //}
+
+        //var scaleTargetNames = new string[] { "Cylinder (1)", "Sphere (1)" };
+        //foreach (var targetName in scaleTargetNames)
+        //{
+        //    var transformGizmo = RTGizmosEngine.Get.CreateObjectScaleGizmo();
+
+        //    GameObject targetObject = GameObject.Find(targetName);
+        //    transformGizmo.SetTargetObject(targetObject);
+        //    transformGizmo.SetTransformSpace(GizmoSpace.Local);
+        //}
+
+        //var universalTargetNames = new string[] { "Blue Cube (1)", "Green Cube" };
+        //foreach (var targetName in universalTargetNames)
+        //{
+        //    var transformGizmo = RTGizmosEngine.Get.CreateObjectUniversalGizmo();
+
+        //    GameObject targetObject = GameObject.Find(targetName);
+        //    transformGizmo.SetTargetObject(targetObject);
+        //    transformGizmo.Gizmo.UniversalGizmo.SetMvVertexSnapTargetObjects(new List<GameObject> { targetObject });
+        //    transformGizmo.SetTransformSpace(GizmoSpace.Local);
+        //}
+
+        //// Create collider gizmos
+        //var colliderObject = GameObject.Find("Cube_BoxCollider");
+        //var gizmo = RTGizmosEngine.Get.CreateGizmo();
+        //var boxColliderGizmo = gizmo.AddBehaviour<BoxColliderGizmo3D>();
+        //boxColliderGizmo.SetTargetCollider(colliderObject.GetComponent<BoxCollider>());
+
+        //colliderObject = GameObject.Find("Sphere_SphereCollider");
+        //gizmo = RTGizmosEngine.Get.CreateGizmo();
+        //var sphereColliderGizmo = gizmo.AddBehaviour<SphereColliderGizmo>();
+        //sphereColliderGizmo.SetTargetCollider(colliderObject.GetComponent<SphereCollider>());
+
+        //colliderObject = GameObject.Find("Capsule_CapsuleCollider");
+        //gizmo = RTGizmosEngine.Get.CreateGizmo();
+        //var capsuleColliderGizmo = gizmo.AddBehaviour<CapsuleColliderGizmo3D>();
+        //capsuleColliderGizmo.SetTargetCollider(colliderObject.GetComponent<CapsuleCollider>());
+
+        //// Create light gizmos
+        //var lightObject = GameObject.Find("Directional Light");
+        //gizmo = RTGizmosEngine.Get.CreateGizmo();
+        //var dirLightGizmo = gizmo.AddBehaviour<DirectionalLightGizmo3D>();
+        //dirLightGizmo.SetTargetLight(lightObject.GetComponent<Light>());
+
+        //lightObject = GameObject.Find("Point Light");
+        //gizmo = RTGizmosEngine.Get.CreateGizmo();
+        //var pointLightGizmo = gizmo.AddBehaviour<PointLightGizmo3D>();
+        //pointLightGizmo.SetTargetLight(lightObject.GetComponent<Light>());
+
+        //lightObject = GameObject.Find("Spot Light");
+        //gizmo = RTGizmosEngine.Get.CreateGizmo();
+        //var spotLightGizmo = gizmo.AddBehaviour<SpotLightGizmo3D>();
+        //spotLightGizmo.SetTargetLight(lightObject.GetComponent<Light>());
+
+        //// Create terrain gizmos
+        //// Note: In order to use the terrain gizmo, you first need to click on the terrain.
+        //var terrainObject = GameObject.Find("Terrain");
+        //gizmo = RTGizmosEngine.Get.CreateGizmo();
+        //var terrainGizmo = gizmo.AddBehaviour<TerrainGizmo>();
+        //terrainGizmo.SetTargetTerrain(terrainObject.GetComponent<Terrain>());
+    }
+
+
+    void Update()
+    {
+        // 检测鼠标点击以选择物体
+        if (Input.GetMouseButtonDown(0)) // 鼠标左键点击
+        {
+            SelectObjectByMouseClick();
+        }
+
+    }
+
+    // 用鼠标点击选中物体
+    void SelectObjectByMouseClick()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);  // 从鼠标位置发射射线
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit))  // 检测射线是否碰撞到物体
+        {
+            selectedObject = hit.collider.gameObject;  // 选中被点击的物体
+            Debug.Log($"选中物体: {selectedObject.name}");  // 输出选中的物体名称
+
+
+            var transformGizmo = RTGizmosEngine.Get.CreateObjectRotationGizmo();
+
+            transformGizmo.SetTargetObject(selectedObject);
+            transformGizmo.SetTransformSpace(GizmoSpace.Local);
+        }
+    }
+
+
+}
---
+++ Assets/Scripts/TransformControl.cs
@@ -0,0 +1,126 @@
+﻿using UnityEngine;
+using RTG;  // 引入 RTGizmos 命名空间
+
+public class TransformControl : MonoBehaviour
+{
+    public LayerMask selectableLayer;  // 公开的LayerMask，可以在外部设置层级
+    private GameObject selectedObject;  // 当前选中的物体
+    private RTGizmosEngine gizmosEngine;  // Gizmo引擎
+    private ObjectTransformGizmo currentTransformGizmo;  // 当前的Transform Gizmo
+    private enum GizmoMode { None, Move, Rotate, Scale } // 用于存储当前的 Gizmo 模式
+    private GizmoMode currentMode = GizmoMode.Move; // 当前操作模式
+
+
+    void Start()
+    {
+        gizmosEngine = RTGizmosEngine.Get;  
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))  
+        {
+            SelectObjectByMouseClick();
+        }
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            currentMode = GizmoMode.Move;   
+            EnableMoveGizmo();   
+        }
+        else if (Input.GetKeyDown(KeyCode.E))
+        {
+            currentMode = GizmoMode.Rotate; 
+            EnableRotateGizmo();  
+        }
+        else if (Input.GetKeyDown(KeyCode.R))
+        {
+            currentMode = GizmoMode.Scale;  
+            EnableScaleGizmo();  
+        }
+    }
+
+    void SelectObjectByMouseClick()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);   
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity, selectableLayer))  
+        {
+            selectedObject = hit.collider.gameObject;   
+            Debug.Log($"选中物体: {selectedObject.name}");   
+
+
+            // 如果当前是 "缩放" 模式，则执行缩放操作，避免触发其他模式
+            if (currentMode == GizmoMode.Move)
+            {
+                EnableMoveGizmo();  // 启用移动Gizmo
+            }
+            else if (currentMode == GizmoMode.Rotate)
+            {
+                EnableRotateGizmo();  // 启用旋转Gizmo
+            }
+            else if (currentMode == GizmoMode.Scale)
+            {
+                EnableScaleGizmo();  // 启用缩放Gizmo
+            }
+        }
+    }
+
+    void EnableMoveGizmo()
+    {
+        if (selectedObject != null)
+        {
+            // 每次切换前，移除当前的 Gizmo
+            if (currentTransformGizmo != null)
+            {
+                gizmosEngine.RemoveGizmo(currentTransformGizmo.Gizmo);  // 移除旧的 Gizmo
+            }
+
+            // 创建并添加新的 Gizmo
+            var transformGizmo = RTGizmosEngine.Get.CreateObjectMoveGizmo();  // 创建移动Gizmo
+            transformGizmo.SetTargetObject(selectedObject);  // 设置选中的物体
+            transformGizmo.SetTransformSpace(GizmoSpace.Local);  // 设置局部变换空间
+            currentTransformGizmo = transformGizmo;  // 设置当前Gizmo为移动Gizmo
+
+        }
+    }
+
+    void EnableRotateGizmo()
+    {
+        if (selectedObject != null)
+        {
+            // 每次切换前，移除当前的 Gizmo
+            if (currentTransformGizmo != null)
+            {
+                gizmosEngine.RemoveGizmo(currentTransformGizmo.Gizmo);  // 移除旧的 Gizmo
+            }
+
+            // 创建并添加新的 Gizmo
+            var transformGizmo = RTGizmosEngine.Get.CreateObjectRotationGizmo();  // 创建旋转Gizmo
+            transformGizmo.SetTargetObject(selectedObject);  // 设置选中的物体
+            transformGizmo.SetTransformSpace(GizmoSpace.Local);  // 设置局部变换空间
+            currentTransformGizmo = transformGizmo;  // 设置当前Gizmo为旋转Gizmo
+
+        }
+    }
+
+    void EnableScaleGizmo()
+    {
+        if (selectedObject != null)
+        {
+            // 每次切换前，移除当前的 Gizmo
+            if (currentTransformGizmo != null)
+            {
+                gizmosEngine.RemoveGizmo(currentTransformGizmo.Gizmo);  // 移除旧的 Gizmo
+            }
+
+            // 创建并添加新的 Gizmo
+            var transformGizmo = RTGizmosEngine.Get.CreateObjectScaleGizmo();  // 创建缩放Gizmo
+            transformGizmo.SetTargetObject(selectedObject);  // 设置选中的物体
+            transformGizmo.SetTransformSpace(GizmoSpace.Local);  // 设置局部变换空间
+            currentTransformGizmo = transformGizmo;  // 设置当前Gizmo为缩放Gizmo
+
+        }
+    }
+}
